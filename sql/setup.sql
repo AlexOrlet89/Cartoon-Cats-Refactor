a2e3f1761c63a7a8a3ff730d6ba7110a3a44bc06
Alex Orlet <AlexOrlet89@gmail.com>
2022-06-08 14:35:54 -0700

    setupsql inserts a row into our cats table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,15 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP table if exists cats;
+
+create table cats (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR NOT NULL,
+    type VARCHAR NOT NULL
+    -- URL VARCHAR NOT NULL,
+    -- year INT NOT NULL,
+    -- lives INT NOT NULL
+);
+
+INSERT INTO cats (name, type) VALUES
+('Felix', 'Tuxedo')
