b959df259e79ce482fe69c220ff6eb9985d368e9
Alex Orlet <AlexOrlet89@gmail.com>
2022-06-08 17:14:23 -0700

    cats data is removed, setup creates friends db, validated on Beekeeper

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,22 +1,20 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP table if exists cats;
+DROP table if exists friends;
 
-create table cats (
+create table friends (
     id BIGINT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR NOT NULL,
-    type VARCHAR NOT NULL
+    status VARCHAR NOT NULL
     -- URL VARCHAR NOT NULL,
     -- year INT NOT NULL,
     -- lives INT NOT NULL
 );
 
-INSERT INTO cats (name, type) VALUES
-('Felix', 'Tuxedo'),
-('Garfield', 'Orange Tabby'),
-('Duchess', 'Angora'),
-('Stimpy', 'Manx'),
-('Sylvester', 'Tuxedo'),
-('Tigger', 'Tiger'),
-('Hello Kitty', 'Angora'),
-('Hobbes', 'Tiger');
+INSERT INTO friends (name, status) VALUES
+('Ross', 'Alive'),
+('Phoebe', 'Alive'),
+('Rachel', 'Alive'),
+('Monica', 'Alive'),
+('Chandler', 'Alive'),
+('Joey', 'Alive')
