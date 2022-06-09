13b51db4c6a76f16236f036722f938766c39fec9
Alex Orlet <AlexOrlet89@gmail.com>
2022-06-09 12:54:57 -0700

    changes to setup sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,20 +1,20 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP table if exists friends;
-
-create table friends (
+DROP table if exists cats;
+create table cats (
     id BIGINT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR NOT NULL,
-    status VARCHAR NOT NULL
+    type VARCHAR NOT NULL
     -- URL VARCHAR NOT NULL,
     -- year INT NOT NULL,
     -- lives INT NOT NULL
 );
-
-INSERT INTO friends (name, status) VALUES
-('Ross', 'Alive'),
-('Phoebe', 'Alive'),
-('Rachel', 'Alive'),
-('Monica', 'Alive'),
-('Chandler', 'Alive'),
-('Joey', 'Alive')
+INSERT INTO cats (name, type) VALUES
+('Felix', 'Tuxedo'),
+('Garfield', 'Orange Tabby'),
+('Duchess', 'Angora'),
+('Stimpy', 'Manx'),
+('Sylvester', 'Tuxedo'),
+('Tigger', 'Tiger'),
+('Hello Kitty', 'Angora'),
+('Hobbes', 'Tiger'); 
