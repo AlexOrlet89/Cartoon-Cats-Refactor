75dc0cdf58b78c2c0ab546f1d5e465382f6759ef
Alex Orlet <AlexOrlet89@gmail.com>
2022-06-10 12:38:02 -0700

    changed to snake casing

--- sql/setup.sql
+++ sql/setup.sql
@@ -8,9 +8,9 @@ create table cats (
     URL VARCHAR NOT NULL,
     year INT NOT NULL,
     lives INT NOT NULL,
-    isSidekick Boolean
+    is_sidekick Boolean
 );
-INSERT INTO cats (name, type, URL, year, lives, isSidekick) VALUES
+INSERT INTO cats (name, type, URL, year, lives, is_sidekick) VALUES
 ('Felix', 'Tuxedo','https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Felix_the_cat.svg/200px-Felix_the_cat.svg.png', 1892, 3, false),
 ('Garfield', 'Orange Tabby','https://static.wikia.nocookie.net/garfield/images/9/9f/GarfieldCharacter.jpg', 1978, 7, false),
 ('Duchess', 'Angora', 'https://static.wikia.nocookie.net/disney/images/e/eb/Profile_-_Duchess.jpeg', 1970, 9, false),
