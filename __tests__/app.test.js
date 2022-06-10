e8ae15746dacf16418020df10fb3f636f82bcdb8
Alex Orlet <AlexOrlet89@gmail.com>
2022-06-10 10:24:45 -0700

    fixed tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,7 +13,7 @@ describe('cats routes', () => {
   it('/cats should return a list of cats', async () => {
     const res = await request(app).get('/cats');
     const expected = cats.map((cat) => {
-      return { id: cat.id, name: cat.name, type: cat.type };
+      return { id: cat.id, name: cat.name };
     });
     expect(res.body).toEqual(expected);
   });
@@ -24,6 +24,9 @@ describe('cats routes', () => {
       id: '1',
       name: 'Felix',
       type: 'Tuxedo',
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Felix_the_cat.svg/200px-Felix_the_cat.svg.png',
+      year: 1892,
+      lives: 3,
     };
     expect(res.body).toEqual(felix);
   });
